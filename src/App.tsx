2d1c0f8bbf7b29437ac65ecff5e5981ec21a7276
footballer2021 <soccerzuki2013@gmail.com>
2022-04-08 20:54:24 +0900

    表題付けて、装飾しました。

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Counter from './components/counter';
 import Navbar from "./components/Navbar";
+import MainPage from './components/MainPage';
 //import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <MainPage />
       <Counter />
     </div>
   );
---
+++ src/components/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Styles/MainPage.css'
+
+function MainPage() {
+  return (
+    <div className='mainPage'>
+        <h1>Welcome to the Website</h1>
+    </div>
+  )
+}
+
+export default MainPage
