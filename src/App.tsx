78bd3f523126eff0bb86f6527f04dc1076cd4b43
footballer2021 <soccerzuki2013@gmail.com>
2022-04-08 19:03:02 +0900

    Navbar starts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import Counter from './components/counter';
+//import Counter from './components/counter';
+import Navbar from "./components/Navbar";
 //import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
       <Counter />
+      <Navbar />
     </div>
   );
 }
